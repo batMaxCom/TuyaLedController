24873470fcf1731a67c1dd32c849fe85a0a68b31
batmaxcom <batmanovv@kamaz.ru>
2026-01-13 15:59:39 +0300

    add first migration

---
+++ infrastructure/persistence/alembic/migrations/env.py
@@ -0,0 +1,94 @@
+from logging.config import fileConfig
+
+from sqlalchemy import engine_from_config
+from sqlalchemy import pool
+
+from alembic import context
+
+from infrastructure.persistence.sqlalchemy.config import PostgresConfig
+from infrastructure.persistence.sqlalchemy.table.base import MAPPER_REGISTRY
+from infrastructure.persistence.sqlalchemy.table.device import DEVICE_TABLE, map_device_table
+
+# this is the Alembic Config object, which provides
+# access to the values within the .ini file in use.
+config = context.config
+
+section = config.config_ini_section
+settings = PostgresConfig.from_env()
+
+# значение, заданное здесь, будет переопределять значение, заданное в alembic.ini
+# значение передается в `ConfigParser.set`, который поддерживает интерполяцию
+# переменных с помощью pyformat (например, `%(some_value)s`).
+config.set_section_option(section, "PG_HOST", settings.host)
+config.set_section_option(section, "PG_PORT", str(settings.port))
+config.set_section_option(section, "PG_USER", settings.user)
+config.set_section_option(section, "PG_DATABASE", settings.db)
+config.set_section_option(section, "PG_PASSWORD", settings.password)
+
+# Interpret the config file for Python logging.
+# This line sets up loggers basically.
+if config.config_file_name is not None:
+    fileConfig(config.config_file_name)
+
+# add your model's MetaData object here
+# for 'autogenerate' support
+# from myapp import mymodel
+# target_metadata = mymodel.Base.metadata
+target_metadata = MAPPER_REGISTRY.metadata
+map_device_table()
+# other values from the config, defined by the needs of env.py,
+# can be acquired:
+# my_important_option = config.get_main_option("my_important_option")
+# ... etc.
+
+
+def run_migrations_offline() -> None:
+    """Run migrations in 'offline' mode.
+
+    This configures the context with just a URL
+    and not an Engine, though an Engine is acceptable
+    here as well.  By skipping the Engine creation
+    we don't even need a DBAPI to be available.
+
+    Calls to context.execute() here emit the given string to the
+    script output.
+
+    """
+    url = config.get_main_option("sqlalchemy.url")
+    context.configure(
+        url=url,
+        target_metadata=target_metadata,
+        literal_binds=True,
+        dialect_opts={"paramstyle": "named"},
+    )
+
+    with context.begin_transaction():
+        context.run_migrations()
+
+
+def run_migrations_online() -> None:
+    """Run migrations in 'online' mode.
+
+    In this scenario we need to create an Engine
+    and associate a connection with the context.
+
+    """
+    connectable = engine_from_config(
+        config.get_section(config.config_ini_section, {}),
+        prefix="sqlalchemy.",
+        poolclass=pool.NullPool,
+    )
+
+    with connectable.connect() as connection:
+        context.configure(
+            connection=connection, target_metadata=target_metadata
+        )
+
+        with context.begin_transaction():
+            context.run_migrations()
+
+
+if context.is_offline_mode():
+    run_migrations_offline()
+else:
+    run_migrations_online()
